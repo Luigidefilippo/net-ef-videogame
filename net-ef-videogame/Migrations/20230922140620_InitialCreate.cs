e9fd0a3b1d7e733e45354fbb86143572400c0967
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-22 16:08:31 +0200

    Create migration done

---
+++ net-ef-videogame/Migrations/20230922140620_InitialCreate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace net_ef_videogame.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Software_houses",
+                columns: table => new
+                {
+                    SoftwarehouseId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Software_houses", x => x.SoftwarehouseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Videogames",
+                columns: table => new
+                {
+                    VideogameId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Overview = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Release_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Software_houseId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Videogames", x => x.VideogameId);
+                    table.ForeignKey(
+                        name: "FK_Videogames_Software_houses_Software_houseId",
+                        column: x => x.Software_houseId,
+                        principalTable: "Software_houses",
+                        principalColumn: "SoftwarehouseId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Videogames_Software_houseId",
+                table: "Videogames",
+                column: "Software_houseId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Videogames");
+
+            migrationBuilder.DropTable(
+                name: "Software_houses");
+        }
+    }
+}
--- net-ef-videogame/Program.cs
+++ net-ef-videogame/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Benvenuto nel gestionale gamestop!");
+
+            while (true)
+            {
+                Console.WriteLine($@"
+1: Insert a new video game.
+2: Search for a video game by its ID.
+3: search for all video games with a name containing a certain input string.
+4: Delete a video game.
+5: Enter a new Software House.
+6: Exit the management system => 
+");
+                Console.Write("Inserisci l'opzione desiderata --> ");
+                int opzioneSelezionata = int.Parse(Console.ReadLine());
+                switch (opzioneSelezionata)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                }
+            }
         }
     }
 }
